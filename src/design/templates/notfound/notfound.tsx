0cb00f48e3a4f5a34af5090407d11d580549d21b
Edinson Nuñez More <nmedinson@gmail.com>
2024-04-23 01:01:10 -0500

    Refactor NotFound component to handle route errors in notfound.tsx

--- src/design/templates/notfound/notfound.tsx
+++ src/design/templates/notfound/notfound.tsx
@@ -1,3 +1,17 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 export function NotFound() {
-  return <div>NOT FOUND!</div>;
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>Oops!</h1>
+        <h2>{error.status}</h2>
+        <p>{error.statusText}</p>
+        {error.data?.message && <p>{error.data.message}</p>}
+      </div>
+    );
+  } else {
+    return <div>Oops</div>;
+  }
 }
