24d0e9c6eeea14883f7e0b42445cf15d0e020928
Edinson Nuñez More <nmedinson@gmail.com>
2024-04-23 01:17:59 -0500

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/edi-developer/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,5 +14,4 @@ export default defineConfig({
       "@design": "/src/design",
     },
   },
-  server: { host: true },
 });
