6d4227fe8a05a97b2310055f431fbcf183a57b66
EdinsonNM <nmedinson@gmail.com>
2024-04-23 05:52:00 +0000

    Deploying to gh-pages from @ EdinsonNM/edi-developer@4e434207d89ade6929718507271f16118628e2dd 🚀

--- src/presentation/home/components/developer.tsx
+++
@@ -1,7 +0,0 @@
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-export function Developer() {
-  const gltf = useLoader(GLTFLoader, "/developer.gltf");
-  return <primitive object={gltf.scene} />;
-}
--- src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-declare module "three/examples/jsm/loaders/GLTFLoader" {
-  const GLTFLoader: any;
-  export { GLTFLoader };
-}
