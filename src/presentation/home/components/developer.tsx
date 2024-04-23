09edacd8b99350b4e28fbe2213bc8a0b90d637d7
Edinson Nuñez More <nmedinson@gmail.com>
2024-04-23 13:16:47 -0500

    Update gltf loader path in Developer component

--- src/presentation/home/components/developer.tsx
+++ src/presentation/home/components/developer.tsx
@@ -2,6 +2,9 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export function Developer() {
-  const gltf = useLoader(GLTFLoader, "/edi-developer/developer.gltf");
+  const gltf = useLoader(
+    GLTFLoader,
+    `${import.meta.env.VITE_ASSETS}developer.gltf`
+  );
   return <primitive object={gltf.scene} />;
 }
