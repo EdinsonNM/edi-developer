c5b6e18e703c3957249ac74d712047bd2439a304
Edinson Nuñez More <nmedinson@gmail.com>
2024-04-23 13:19:07 -0500

    Update developer import in Developer component

--- src/presentation/home/components/developer.tsx
+++ src/presentation/home/components/developer.tsx
@@ -2,9 +2,6 @@ import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export function Developer() {
-  const gltf = useLoader(
-    GLTFLoader,
-    `${import.meta.env.VITE_ASSETS}developer.gltf`
-  );
+  const gltf = useLoader(GLTFLoader, `./developer.gltf`);
   return <primitive object={gltf.scene} />;
 }
