d15d15b2649945704a3d0c764ab7ddb7253ac31b
Edinson Nuñez More <nmedinson@gmail.com>
2024-07-09 02:27:32 -0500

    feat: Update text color in Stats component

--- src/presentation/home/components/stats.tsx
+++ src/presentation/home/components/stats.tsx
@@ -9,7 +9,7 @@ function Stats() {
     <div className="flex flex-row gap-2 md:gap-10 mt-6 flex-wrap items-center justify-center md:justify-end">
       {items.map((item) => (
         <div key={item.title}>
-          <h3 className="text-sm text-orange-500 w-28 text-center mb-3">
+          <h3 className="text-sm text-gray-500 w-28 text-center mb-3">
             {item.title}
           </h3>
           <p className="text-3xl md:text-5xl text-center">{item.value}</p>
