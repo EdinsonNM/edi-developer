dbb425568fd1c636aa7aff30ea4048f83c12e86e
EdinsonNM <EdinsonNM@users.noreply.github.com>
2024-11-18 07:20:35 +0000

    Deploying to gh-pages from @ EdinsonNM/edi-developer@42c830d6fb22b538d9df8140a8394b89380519cc 🚀

---
+++ assets/button-CnszqlX2.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CZNtE5tY.js";function r({children:t}){return e.jsx("a",{className:"text-center mt-2 block bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-700 transition-colors pointer-events-all cursor-pointer",children:t})}export{r as B};
